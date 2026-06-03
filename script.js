78134cad7787fc1ebd58918060374412ea7bf8ba
UnnarDarri <79046964+UnnarDarri@users.noreply.github.com>
2026-06-03 23:12:29 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,6 @@
-// LENIS smooth scroll
-const lenis = new Lenis()
-
-function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
-requestAnimationFrame(raf)
-
-
-// GSAP animations
-gsap.registerPlugin(ScrollTrigger)
-
-// fade in sections
-gsap.utils.toArray("section").forEach(section => {
-  gsap.from(section, {
-    opacity: 0,
-    y: 80,
-    duration: 1,
-    scrollTrigger: {
-      trigger: section,
-      start: "top 80%"
-    }
-  })
-})
-
-// loader animation
 window.addEventListener("load", () => {
-  gsap.to("#loader", {
-    opacity: 0,
-    duration: 1,
-    delay: 1,
-    onComplete: () => document.getElementById("loader").remove()
-  })
+  const loader = document.getElementById("loader")
+  if (loader) {
+    loader.style.display = "none"
+  }
 })
