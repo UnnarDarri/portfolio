e4deaeeb2ba4318dffd63218549402dd4463e2c0
Reyn Alpha <reynalpha@outlook.com>
2026-06-07 19:19:36 +0000

    Standardize quotes and indentation

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,12 @@
-import { HtmlBasePlugin } from '@11ty/eleventy';
+import { HtmlBasePlugin } from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
-    eleventyConfig.addPlugin(HtmlBasePlugin);
-    eleventyConfig.addPassthroughCopy("static");
-    eleventyConfig.addPassthroughCopy("css");
-    eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPlugin(HtmlBasePlugin);
+  eleventyConfig.addPassthroughCopy("static");
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
 }
 
 export const config = {
-    pathPrefix: '/portfolio',
+  pathPrefix: "/portfolio",
 }
