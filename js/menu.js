8425daebcb2bc9295a6f5ba07f0d11f08cd31b29
Reyn Alpha <reynalpha@outlook.com>
2026-06-12 18:51:50 +0000

    Add mobile styles and menu

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+const menuToggle = document.getElementById("mobile-menu-button");
+const menuText = menuToggle.querySelector(".span");
+const nav = document.getElementById("main-nav");
+
+menuToggle.addEventListener("click", () => {
+  if (nav.classList.contains("mobile-menu-visible")) {
+    nav.classList.remove("mobile-menu-visible");
+    menuToggle.ariaExpanded = false;
+    menuText.textContent = "Expand menu";
+  } else {
+    nav.classList.add("mobile-menu-visible");
+    menuToggle.ariaExpanded = true;
+    menuText.textContent = "Close menu";
+  }
+});
