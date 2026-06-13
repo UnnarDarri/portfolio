62031b800c5ceafcffb0e5e19cd9811c6d5e2f86
Reyn Alpha <reynalpha@outlook.com>
2026-06-13 21:40:27 +0000

    Move video id to constant

--- js/home.js
+++ js/home.js
@@ -1,9 +1,10 @@
 const playButton = document.getElementById("showreel-play-button");
 const playerContainer = document.getElementById("showreel-container");
+const VIDEO_ID = "WCzgM3aWdXU";
 
 playButton.addEventListener("click", () => {
     const iframe = document.createElement("iframe");
-    iframe.src = "https://www.youtube.com/embed/WCzgM3aWdXU?autoplay=1&playsinline=0";
+    iframe.src = `https://www.youtube.com/embed/${VIDEO_ID}?autoplay=1&playsinline=0`;
     iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
     iframe.referrerPolicy = "strict-origin-when-cross-origin";
     iframe.allowFullscreen = true;
