27330ca4330beb7715cf9dcc1e6af2ece17e1baa
Reyn Alpha <reynalpha@outlook.com>
2026-06-13 21:56:16 +0000

    Remove third party recommendations from YouTube embeds

--- js/home.js
+++ js/home.js
@@ -4,7 +4,7 @@ const VIDEO_ID = "WCzgM3aWdXU";
 
 playButton.addEventListener("click", () => {
     const iframe = document.createElement("iframe");
-    iframe.src = `https://www.youtube.com/embed/${VIDEO_ID}?autoplay=1&playsinline=0`;
+    iframe.src = `https://www.youtube.com/embed/${VIDEO_ID}?autoplay=1&playsinline=0&rel=0`;
     iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
     iframe.referrerPolicy = "strict-origin-when-cross-origin";
     iframe.allowFullscreen = true;
