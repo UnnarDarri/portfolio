206be7b6c0236ff325bd2f84007cbc6bd0b75ce5
Reyn Alpha <reynalpha@outlook.com>
2026-06-13 21:39:56 +0000

    Play showreel in full screen on iOS

--- js/home.js
+++ js/home.js
@@ -3,7 +3,7 @@ const playerContainer = document.getElementById("showreel-container");
 
 playButton.addEventListener("click", () => {
     const iframe = document.createElement("iframe");
-    iframe.src = "https://www.youtube.com/embed/WCzgM3aWdXU?autoplay=1";
+    iframe.src = "https://www.youtube.com/embed/WCzgM3aWdXU?autoplay=1&playsinline=0";
     iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
     iframe.referrerPolicy = "strict-origin-when-cross-origin";
     iframe.allowFullscreen = true;
